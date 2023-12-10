73b2fedb4c66bd15871ef3fd3fca1170e89507e9
Maksym Shokin <problaze7@gmail.com>
2023-12-10 21:43:45 +0200

    Added image picker

---
+++ app/meals/[slug]/not-found.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Recipe not found</h1>
+      <p>Could not find requested page or resource</p>
+    </main>
+  );
+}
