0555963c1ebf5842a4e025d257ce757e2418ab95
Maksym Shokin <problaze7@gmail.com>
2023-12-10 11:24:22 +0200

    error-page

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error(props) {
+  return (
+    <main className='error'>
+      <h1>An error occurred</h1>
+      <p>Fetching meals failed. Try again later</p>
+    </main>
+  );
+}
